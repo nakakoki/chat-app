038cdd107c455e61f45169230cf82743c11c5d09
nakakoki <71761751+nakakoki@users.noreply.github.com>
2020-09-30 11:38:52 +0900

    画面遷移の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,7 @@
 class RoomsController < ApplicationController
+  def index
+  end
+  
   def new
     @room = Room.new
   end
@@ -15,6 +18,6 @@ class RoomsController < ApplicationController
   private
 
   def room_params
-    params.require(:room).permit(:name, user_ids: [])
+    params.require(:room).permit(:name, user_ids:[])
   end
 end
